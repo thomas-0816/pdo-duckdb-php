ef5c6cb63ff6ceb806eb1f2de3f761f9dd9ecdb9
Thomas Bley <mails@thomasbley.de>
2026-07-04 02:51:25 +0200

    added parallel testing

---
+++ test_parallel.php
@@ -0,0 +1,33 @@
+<?php
+
+$start = microtime(true);
+for ($i = 0; $i < 64; $i++) {
+    ob_start();
+    require 'test.php';
+    ob_end_clean();
+    echo '.';
+}
+echo 'DONE ', microtime(true) - $start, PHP_EOL;
+
+$start = microtime(true);
+$runtimes = [];
+for ($i = 0; $i < 64; $i++) {
+    $runtimes[] = new parallel\Runtime();
+}
+$futures = [];
+for ($i = 0; $i < 64; $i++) {
+    $futures[] = $runtimes[$i]->run(function() use ($i) {
+        $start = (new DateTime())->format('H:i:s.u');
+        ob_start();
+        require 'test.php';
+        ob_end_clean();
+        $end = (new DateTime())->format('H:i:s.u');
+        echo '.';
+
+        return $start . ' ' . $end . PHP_EOL;
+    });
+}
+foreach ($futures as $future) {
+    $future->value();
+}
+echo 'DONE ', microtime(true) - $start, PHP_EOL;
