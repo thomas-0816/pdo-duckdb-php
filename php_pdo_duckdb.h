ee587f233772aff3941e663682568741a54d59f8
Thomas Bley <mails@thomasbley.de>
2026-07-16 23:18:04 +0200

    cleanup

--- php_pdo_duckdb.h
+++ php_pdo_duckdb.h
@@ -21,8 +21,12 @@ ZEND_TSRMLS_CACHE_EXTERN()
 
 /* Module globals – required by ZEND_DECLARE_MODULE_GLOBALS */
 ZEND_BEGIN_MODULE_GLOBALS(pdo_duckdb)
-	int dummy;
+	zif_handler original_pdo_stmt_execute;
 ZEND_END_MODULE_GLOBALS(pdo_duckdb)
+
+ZEND_EXTERN_MODULE_GLOBALS(pdo_duckdb)
+
+#define PDO_DUCKDB_G(v) ZEND_MODULE_GLOBALS_ACCESSOR(pdo_duckdb, v)
 
 /* Forward declarations of the driver and statement method tables.
    Their implementations are in duckdb_driver.c and duckdb_statement.c */
