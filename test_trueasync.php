feb12dda1da5d0a0f04f188978561d8a839e425f
Thomas Bley <mails@thomasbley.de>
2026-07-30 22:12:48 +0200

    extended examples for trueasync

--- test_trueasync.php
+++ test_trueasync.php
@@ -48,7 +48,7 @@ echo '5x sleep 0.5s took ', round(microtime(true) - $start, 2), 's', PHP_EOL;
 // run write in parallel using threads, on-disk
 @unlink('/tmp/test.duckdb');
 $pdo = new PDO('duckdb:/tmp/test.duckdb');
-$pdo->exec('create table if not exists orders (user_id integer primary key)');
+$pdo->exec('create table orders (user_id integer primary key)');
 $result = $pdo->query("CALL quack_serve('quack:127.0.0.1:9494')")->fetch(PDO::FETCH_ASSOC);
 // print_r($result);
 $authToken = $result['auth_token'];
@@ -67,20 +67,24 @@ $pdo->exec("CALL quack_stop('quack:127.0.0.1')");
 echo json_encode($pdo->query("SELECT * from orders")->fetchAll(PDO::FETCH_COLUMN)), PHP_EOL;
 
 
-// run read in parallel using threads, on-disk
+// run read and write in parallel using coroutines, on-disk
 @unlink('/tmp/test.duckdb');
 $pdo = new PDO('duckdb:/tmp/test.duckdb');
-$pdo->exec('create table if not exists orders (user_id integer primary key)');
-for ($i = 0; $i < 10; $i++) {
-    $pdo->exec("INSERT INTO orders (user_id) VALUES ($i)");
-}
+$pdo->exec('create table orders (user_id integer primary key)');
 $threads = [];
+$coroutines = [];
 for ($i = 0; $i < 10; $i++) {
-    $threads[] = Async\spawn_thread(function() use ($i) {
+    $threads[] = Async\spawn_thread(function() {
+        Async\delay(rand(5, 20));
         $pdo = new PDO('duckdb:/tmp/test.duckdb', null, null, [PDO::DUCKDB_ATTR_CONFIG => ['access_mode' => 'read_only']]);
         return json_encode($pdo->query("SELECT * from orders")->fetchAll(PDO::FETCH_COLUMN));
     });
+    $coroutines[] = Async\spawn(function() use ($i, $pdo) {
+        Async\delay(rand(0, 20));
+        $pdo->exec("INSERT INTO orders (user_id) VALUES ($i)");
+    });
 }
+Async\await_all($coroutines);
 [$result, $errors] = Async\await_all($threads);
 print_r($result);
 
