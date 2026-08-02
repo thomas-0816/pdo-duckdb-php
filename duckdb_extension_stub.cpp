9d15667dcade920c870efe7136d293dad5985317
Thomas Bley <mails@thomasbley.de>
2026-08-02 20:04:14 +0200

    preload core functions

--- duckdb_extension_stub.cpp
+++ duckdb_extension_stub.cpp
@@ -5,9 +5,11 @@ namespace duckdb {
 class ExtensionHelper {
 public:
 	static void LoadAllExtensions(DuckDB &db);
+	static bool TryAutoLoadExtension(DatabaseInstance &db, const std::string &extension_name) noexcept;
 };
 
-void ExtensionHelper::LoadAllExtensions(DuckDB &) {
+void ExtensionHelper::LoadAllExtensions(DuckDB &db) {
+	ExtensionHelper::TryAutoLoadExtension(*db.instance, "core_functions");
 }
 
 } // namespace duckdb
